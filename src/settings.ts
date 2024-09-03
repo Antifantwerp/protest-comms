f5a31fa4cae63c43b103cc6abe15e0630879d059
Pigeon <antifantwerp@proton.me>
2024-09-03 17:19:02 +0200

    Fixed multiple new slogan inputs

--- src/settings.ts
+++ src/settings.ts
@@ -12,13 +12,15 @@ $(window).on("load", () => {
     addSlogan.on("click", (e) => {
         if (!addingSlogan) {
             $("#edit-slogans")
-                .prepend(`<label for="new-slogan" />`)
+                .prepend(`<label for="new-slogan" id="new-slogan-label" />`)
                 .prepend(`<input type="text" id="new-slogan" name="new-slogan" placeholder="New slogan text..." />`)
             addSlogan.val("Save new slogan");
         } else {
             pb.collection("slogans").create({
                 text: $("#new-slogan").val()
             });
+            $("#new-slogan").remove();
+            $("#new-slogan-label").remove();
         }
         addingSlogan = !addingSlogan;
     })
